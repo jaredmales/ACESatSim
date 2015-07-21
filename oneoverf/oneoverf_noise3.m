bee81c46b81df45b3547ad8e723cab0402659cd8
Jared Males <jaredmales@gmail.com>
2015-07-21 15:40:56 -0700

    updated oneoverf_noise3 to handle large cubes, and added oneoverf_gridgen.m to batch-generated oneoverf cubes.  Also added simple status_line.m utility'

--- oneoverf/oneoverf_noise3.m
+++ oneoverf/oneoverf_noise3.m
@@ -70,33 +70,80 @@ end
 
 
 Nspace = space_nsamps*space_oversamp;
-Ntime = time_nsamps*time_oversamp;
 
-%Allocate the working cube
-cube = zeros(space_nsamps, space_nsamps, Ntime);
+%The time series is typically longer, and we make it a power of 2
+% to make fft as efficient as possible.
+Ntime = 2^nextpow2(time_nsamps*time_oversamp);
 
-%Now generate each plane of the cube
-for i=1:Ntime 
-   cube(:,:,i) = oneoverf_noise2(space_nsamps, space_oversamp, space_alpha, 'dist', distspec);
+
+%There are two ways to do this:
+% -- start by generating a time series for each pixel in the oversampled planes, 
+% then filter each plane (order==0)
+% -- start by generating a plane for each point in the oversampled time
+% series, then filter the time series (order == 1)
+%
+% Choice depends on memory usage for the cube temporary
+order = 0;
+
+if (Nspace*Nspace*time_nsamps > space_nsamps*space_nsamps*Ntime)
+    order = 1;
 end
 
+%For status_line
+laststr = 0;
+
+if (order==0)
+
+    %Allocate the working cube, single precision to save
+    %space
+    cube = zeros(Nspace, Nspace, time_nsamps, 'single');
+
+    %Now allocate the final cube
+    cnoise = zeros(space_nsamps, space_nsamps, time_nsamps, 'single');
 
 
-%Now allocate the final cube
-cnoise = zeros(space_nsamps, space_nsamps, time_nsamps);
+    %Generate a time series for each pixel in the oversampled cube
+    
+    for i=1:Nspace
+        for j=1:Nspace
+            laststr = status_line(sprintf('%d/%d %d/%d', i, Nspace, j, Nspace), laststr);
+            
+            
+            cube(i,j,:) = oneoverf_noise(time_nsamps, time_oversamp, time_alpha);
+        end
+    end
 
-%And filter and extract each time series
-for i=1:space_nsamps
-   for j=1:space_nsamps
-      cnoise(i,j,:) = oneoverf_noise(time_nsamps, time_oversamp, time_alpha, reshape(cube(i,j,:),1,[]));
-   end
+    %Now filter and extract each plane
+    for i=1:time_nsamps 
+        laststr = status_line(sprintf('%d/%d', i, time_nsamps), laststr);
+        
+        cnoise(:,:,i) = oneoverf_noise2(space_nsamps, space_oversamp, space_alpha, cube(:,:,i));
+    end
+
+else
+
+    %Allocate the working cube, in single precision to save space
+    cube = zeros(space_nsamps, space_nsamps, Ntime, 'single');
+ 
+    %Now allocate the final cube
+    cnoise = zeros(space_nsamps, space_nsamps, time_nsamps, 'single');
+ 
+    %Now generate each plane of the cube
+    for i=1:Ntime 
+        laststr = status_line(sprintf('%d/%d\n', i, Ntime), laststr);
+        cube(:,:,i) = oneoverf_noise2(space_nsamps, space_oversamp, space_alpha, 'dist', distspec);
+    end
+
+    %And filter and extract each time series
+    for i=1:space_nsamps
+        for j=1:space_nsamps
+            laststr = status_line(sprintf('%d/%d %d/%d\n', i, space_nsamps, j, space_nsamps));
+            cnoise(i,j,:) = oneoverf_noise(time_nsamps, time_oversamp, time_alpha, double(reshape(cube(i,j,:),1,[])));
+        end
+    end
 end
 
-
-
-
-
-
+status_line('', laststr);
 
 end
 
